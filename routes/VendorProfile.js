ba8c264701a30ae168b5dd7b17f946bbf7b9cd58
Maulina22 <maulin2210@gmail.com>
2022-03-10 23:44:25 +0530

    vendor_profile_function

--- routes/VendorProfile.js
+++ routes/VendorProfile.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const db = require('./db')
 
 
-const emailref="vendor2@gmail.com"
 
 
 const VendorGet = (req,res) => {
-        
-    const sql = `SELECT state,city,area,address,charge,phoneno FROM vendor where email="${emailref}"`;
+    const id = parseInt(req.params.id);
+    const sql = `SELECT state,city,area,address,charge,phoneno,name FROM vendor where v_id=${id}`;
     const query = db.query(sql,(err,result)=>{
+        console.log(id);
         if(err) throw err;
         // res.json(query);
        res.send(result);
@@ -27,10 +27,11 @@ const VendorPost = (req, res) => {
         charge: req.body.charge,
         area: req.body.area,
         address: req.body.address,
-        phoneno:req.body.phoneno
+        phoneno:req.body.phoneno,
+        name: req.body.name
     };
-    
-    const sql = `UPDATE vendor SET ? WHERE email="${emailref}"`;
+    const id = parseInt(req.params.id);
+    const sql = `UPDATE vendor SET ? WHERE v_id=${id}`;
         const query = db.query(sql,data,(err,result)=>{
             if(err) throw err;
             res.send("yes");
@@ -40,9 +41,9 @@ const VendorPost = (req, res) => {
 }
 
 
-router.route('/vendorprofile').put(VendorPost)
+router.route('/vendorprofile/:id').put(VendorPost)
 
-router.route('').get(VendorGet)
+router.route('/:id').get(VendorGet)
 
 
 
