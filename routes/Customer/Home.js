a56b15e00647843728a96cf81cec08ae15d816b4
astha9120 <201801169@daiict.ac.in>
2022-04-13 17:35:10 +0530

    Notification

--- routes/Customer/Home.js
+++ routes/Customer/Home.js
@@ -18,13 +18,46 @@ const NewsPaper = (req,res)=>{
     })
 }
 
-const GetNoti = ()=>{
+const GetNoti = (req,res)=>{
     const id = req.params.id;
     const sql = `SELECT log FROM customer WHERE c_id=${id}`
+    let Nowdate =  new Date()
+    // console.log(Nowdate)
+
     let notifications = []
     const q = db.query(sql,(err,result)=>{
+        console.log(result)
         if(result[0].log==1)
             notifications.push("Newspaper has been delivered")
+        
+        const sql2 = `SELECT date FROM orders WHERE subscribe = 1 and c_id=${id}`
+        const q2 = db.query(sql2,(err,result2)=>{
+            console.log(result2)
+            let fi = result2.map(e=>{
+                //console.log(e.date)
+                let d = new Date(e.date)
+                console.log(d)
+                let diff = Math.abs(Nowdate-d)
+                diff= Math.floor(diff/(1000 * 3600 * 24))
+                console.log(diff)
+                if(diff>=27 && diff<=30)
+                    return e;
+            })
+            if(fi.length>=1)
+                notifications.push("Your Subscription will be expired in few days")
+            res.send(notifications)
+        })
+
+        
+    })
+}
+
+const MarkRead = (req,res)=>{
+    const id = req.params.id;
+    const sql = `UPDATE customer SET log = 0 WHERE c_id=${id}`
+    const q = db.query(sql,(err,result)=>{
+        console.log(result)
+        res.send("Done")
     })
 }
 
@@ -33,5 +66,6 @@ router.route('/:id')
 
 router.route('/daily/:id')
 .get(GetNoti)
+.post(MarkRead)
 
 module.exports = router;
