2b4094d57cb3e54150a6feac09139f9cfe729818
astha9120 <201801169@daiict.ac.in>
2022-04-15 17:28:22 +0530

    notification

--- routes/Customer/Home.js
+++ routes/Customer/Home.js
@@ -30,10 +30,11 @@ const GetNoti = (req,res)=>{
         if(result[0].log==1)
             notifications.push("Newspaper has been delivered")
         
-        const sql2 = `SELECT date FROM orders WHERE subscribe = 1 and c_id=${id}`
+        let flag_expired =0 ,flag_bill=0; 
+        const sql2 = `SELECT date,bill_status FROM orders WHERE subscribe = 1 and c_id=${id} and log=1`
         const q2 = db.query(sql2,(err,result2)=>{
             console.log(result2)
-            let fi = result2.map(e=>{
+            result2.map(e=>{
                 //console.log(e.date)
                 let d = new Date(e.date)
                 console.log(d)
@@ -41,10 +42,18 @@ const GetNoti = (req,res)=>{
                 diff= Math.floor(diff/(1000 * 3600 * 24))
                 console.log(diff)
                 if(diff>=27 && diff<=30)
-                    return e;
+                    flag_expired=1;
+                if(diff>30 && e.bill_status==0 && diff<=35)
+                    flag_bill=1;
             })
-            if(fi.length>=1)
+
+            if(flag_expired==1)
                 notifications.push("Your Subscription will be expired in few days")
+            if(flag_bill==1){
+                notifications.push("Your subscription is expired , renew it now if you haven't done it")
+                notifications.push("Please pay the bill to your delivery boy")
+
+            }
             res.send(notifications)
         })
 
@@ -57,7 +66,13 @@ const MarkRead = (req,res)=>{
     const sql = `UPDATE customer SET log = 0 WHERE c_id=${id}`
     const q = db.query(sql,(err,result)=>{
         console.log(result)
-        res.send("Done")
+
+        const sql2 = `UPDATE orders SET log = 0 WHERE c_id = ${id}`
+        const q2 = db.query(sql2,(err,result2)=>{
+            console.log(result2)
+            res.send("done")
+        })
+        
     })
 }
 
