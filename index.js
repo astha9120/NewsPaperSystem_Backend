34ac664d1f6279a5b89ed41153d3a5437352236d
astha9120 <201801169@daiict.ac.in>
2022-05-01 21:03:25 +0530

    CORS

--- index.js
+++ index.js
@@ -28,12 +28,7 @@ const Ndblist = require("./routes/vendor/Ndblist")
 const app = express();
 
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
