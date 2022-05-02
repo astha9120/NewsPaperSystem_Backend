a15a10edd42e03e9cdbd10b790ff36c91ce0b86f
Maulina22 <maulin2210@gmail.com>
2022-05-02 22:32:41 +0530

    change

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const app = express();
 
 
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
