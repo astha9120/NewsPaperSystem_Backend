341c18d1a36bee0b9c461cf5a13e0ee5472388e5
Maulina22 <maulin2210@gmail.com>
2022-05-06 12:47:21 +0530

    changes

--- routes/db.js
+++ routes/db.js
@@ -1,23 +1,38 @@
-const mysql = require("mysql2")
+// const mysql = require("mysql2")
 
 
-// Database connection 
+// // Database connection 
 
-const db = mysql.createConnection({
-    host:'localhost',
-    user:process.env.db_user,
-    password:process.env.db_password,
-    database:'newspapersystem'
-})
+// const db = mysql.createConnection({
+//     host:'localhost',
+//     user:process.env.db_user,
+//     password:process.env.db_password,
+//     database:'newspapersystem'
+// })
 
-db.connect((err)=>{
-    if(err){
-        throw err;
-    }
-    console.log("mysql connected")
-})
+// db.connect((err)=>{
+//     if(err){
+//         throw err;
+//     }
+//     console.log("mysql connected")
+// })
 
-module.exports = db
+// module.exports = db
+
+const Pool = require("pg").Pool;
+
+const pool  = new Pool({
+    user : "postgres",
+    password:"maulina",
+    host:"localhost",
+    port:5432,
+    database:"newsdaily" 
+});
+
+module.exports = pool;
+ 
+
+
 
 
 
